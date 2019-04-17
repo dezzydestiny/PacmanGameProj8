1545a102ff92a109c0dcd5e6cb6b1b332da6d6fd
ezekywok <jason.ham.awesome@gmail.com>
2019-04-17 05:42:20 -0400

    updated the images

--- src/Game.java
+++ src/Game.java
@@ -1,107 +1,100 @@
 public class Game
 {
-  private Grid grid;
-  private int userRow;
-  private int userCol;
-  private int msElapsed;
-  private int timesGet;
-  private int timesAvoid;
+    private Grid grid;
+    private int userRow;
+    private int userCol;
+    private int msElapsed;
+    private int timesGet;
+    private int timesAvoid;
 
-  public Game()
-  {
-    grid = new Grid(10, 10);
-    userRow = 0;
-    userCol =  0;
-    msElapsed = 0;
-    timesGet = 0;
-    timesAvoid = 0;
-    updateTitle();
-    grid.setImage(new Location(userRow, 0), "user.gif");
-    //setImage(Location loc, String imageFileName)
-  }
-  public void handleKeyPress()
-  {
-    int key = grid.checkLastKeyPressed();
-    if(userRow > 0 && userRow<=100 && key == 38) //up
+    public Game()
     {
-      grid.setImage(new Location(userRow, userCol), null);
-      userRow--;
-      grid.setImage(new Location(userRow, userCol), "user.gif");
+        grid = new Grid(10, 10);
+        userRow = 0;
+        userCol =  0;
+        msElapsed = 0;
+        timesGet = 0;
+        timesAvoid = 0;
+        updateTitle();
+        grid.setImage(new Location(userRow, 0), "pacRight.PNG");
+        //setImage(Location loc, String imageFileName)
     }
-    else if(userRow < 100 &&userRow>=0 && key == 40)  //down
+    public void handleKeyPress()
     {
-      grid.setImage(new Location(userRow, userCol), null);
-      userRow++;
-      grid.setImage(new Location(userRow, userCol), "user.gif");
+        int key = grid.checkLastKeyPressed();
+        if(userRow > 0 && userRow<=10 && key == 38) //up
+        {
+            grid.setImage(new Location(userRow, userCol), null);
+            userRow--;
+            grid.setImage(new Location(userRow, userCol), "pacUp.PNG");
+        }
+        else if(userRow < 10 &&userRow>=0 && key == 40)  //down
+        {
+            grid.setImage(new Location(userRow, userCol), null);
+            userRow++;
+            grid.setImage(new Location(userRow, userCol), "pacDown.PNG");
+        }
+        else if(userCol>0 && userCol <= 10 && key == 37)  //left
+        {
+            grid.setImage(new Location(userRow, userCol), null);
+            userCol--;
+            grid.setImage(new Location(userRow, userCol), "pacLeft.PNG");
+        }
+        else if(userCol < 10 && userCol>=0 && key == 39)  //right
+        {
+            grid.setImage(new Location(userRow, userCol), null);
+            userCol++;
+            grid.setImage(new Location(userRow, userCol), "pacRight.PNG");
+        }
     }
-    else if(userCol>0 && userCol <= 100 && key == 37)  //left
+
+    public void play()
     {
-      grid.setImage(new Location(userRow, userCol), null);
-      userCol--;
-      grid.setImage(new Location(userRow, userCol), "user.gif");
+        while (!isGameOver())
+        {
+            grid.pause(100);
+            handleKeyPress();
+            updateTitle();
+            msElapsed += 100;
+        }
     }
-    else if(userCol < 100 && userCol>=0 && key == 39)  //right
+
+
+    public void populateRightEdge()
     {
-      grid.setImage(new Location(userRow, userCol), null);
-      userCol++;
-      grid.setImage(new Location(userRow, userCol), "user.gif");
     }
-    else
-      grid.setImage(new Location(userRow, userCol), "user.gif");
-  }
 
-  public void play()
-  {
-    while (!isGameOver())
+    public void scrollLeft()
     {
-      grid.pause(100);
-      handleKeyPress();
-      if (msElapsed % 300 == 0)
-      {
-        scrollLeft();
-        populateRightEdge();
-      }
-      updateTitle();
-      msElapsed += 100;
     }
-  }
 
+    public void handleCollision(Location loc)
+    {
+    }
 
-  public void populateRightEdge()
-  {
-  }
+    public int getScore()
+    {
+        return 0;
+    }
 
-  public void scrollLeft()
-  {
-  }
+    public void updateTitle()
+    {
+        grid.setTitle("Game:  " + getScore());
+    }
 
-  public void handleCollision(Location loc)
-  {
-  }
+    public boolean isGameOver()
+    {
+        return false;
+    }
 
-  public int getScore()
-  {
-    return 0;
-  }
+    public static void test()
+    {
+        Game game = new Game();
+        game.play();
+    }
 
-  public void updateTitle()
-  {
-    grid.setTitle("Game:  " + getScore());
-  }
-
-  public boolean isGameOver()
-  {
-    return false;
-  }
-
-  public static void test()
-  {
-    Game game = new Game();
-    game.play();
-  }
-
-  public static void main(String[] args)
-  {
-    Game.test();
-  }
+    public static void main(String[] args)
+    {
+        Game.test();
+    }
 }
